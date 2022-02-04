9347301a3fca153db796343b584d87c7dfb8957d
Arvid <biskus@gmail.com>
2022-02-04 08:45:38 +0100

    Added keyboard move functionality

--- script.js
+++ script.js
@@ -5,5 +5,28 @@ var canvas = document.getElementById('mittCanvas');
 
 var ctx = canvas.getContext("2d");
 
-ctx.fillRect(0,0,50,50);
 
+var x = 0;
+var y = 0;
+var sizeX = 50;
+var sizeY = 50;
+
+var canvasHeight = canvas.height;
+var canvasWidth = canvas.width;
+
+
+
+
+document.addEventListener('keydown',function(e){
+    console.log(e);
+    if(e.key == 'ArrowUp'){
+        y -= 10;
+    }else if(e.key == 'ArrowDown'){
+        y += 10;
+    }else if(e.key == 'ArrowRight'){
+        x += 10;
+    }else if(e.key == 'ArrowLeft'){
+        x -= 10;
+    }
+    ctx.fillRect(x,y,sizeX, sizeY);
+});
